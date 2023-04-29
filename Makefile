3ec2df476251aa6f18e66f4106d70867427d7e6d
Aram Hamo <contact@aramhamo.me>
2023-04-29 15:46:54 +0200

    modified:   Makefile modified:   src/totp-cli modified:   src/totp-cli.bash

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 install:
 	sudo cp src/totp-cli /usr/local/bin/
-	sudo cp src/totp-cli.bash /usr/share/bash-completion/completions/
+	sudo cp src/totp-cli.bash /usr/share/bash-completion/completions/totp-cli
--- src/totp-cli.bash
+++ src/totp-cli.bash
@@ -1,11 +1,11 @@
 _totp-cli(){
-  local options="help init generate new"
+  local options="help init generate new remove"
 
   if [ "$COMP_CWORD" -eq 1 ]; then
     COMPREPLY=($(compgen -W "$options" -- ${COMP_WORDS[COMP_CWORD]}))
 
   elif [ "$COMP_CWORD" -eq 2 ]; then
-    if [ "${COMP_WORDS[1]}" == "generate" ]; then
+    if ( [ "${COMP_WORDS[1]}" == "generate" ] || [ "${COMP_WORDS[1]}" == "remove" ] ) ; then
       local migrations="`'ls' ~/.local/share/totp-cli/`"
       COMPREPLY=($(compgen -W "$migrations" -- ${COMP_WORDS[2]}))
 
