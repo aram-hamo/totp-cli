b39dc466ae22117b57e412316351ab0296c29deb
Aram Hamo <contact@aramhamo.me>
2023-05-04 09:25:16 +0200

    modified:   src/totp-cli modified:   src/totp-cli.bash

--- src/totp-cli.bash
+++ src/totp-cli.bash
@@ -1,11 +1,11 @@
 _totp-cli(){
-  local options="help init generate new remove"
+  local options="help init generate new remove get-secret"
 
   if [ "$COMP_CWORD" -eq 1 ]; then
     COMPREPLY=($(compgen -W "$options" -- ${COMP_WORDS[COMP_CWORD]}))
 
   elif [ "$COMP_CWORD" -eq 2 ]; then
-    if ( [ "${COMP_WORDS[1]}" == "generate" ] || [ "${COMP_WORDS[1]}" == "remove" ] ) ; then
+    if ( [ "${COMP_WORDS[1]}" == "generate" ] || [ "${COMP_WORDS[1]}" == "remove" ] || [ "${COMP_WORDS[1]}" == "get-secret" ] ) ; then
       local migrations="`'ls' ~/.local/share/totp-cli/`"
       COMPREPLY=($(compgen -W "$migrations" -- ${COMP_WORDS[2]}))
 
