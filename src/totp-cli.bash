1adde254b982493964aaac7818ae0b6ec9f8d92c
Aram Hamo <contact@aramhamo.me>
2024-03-20 13:13:05 +0100

    Big changes
    - Seperate config and Accounts directory
    - using fzf to select when no argument is given after options ```remove``` ```generate``` and ```get-secret```

--- src/totp-cli.bash
+++ src/totp-cli.bash
@@ -6,7 +6,7 @@ _totp-cli(){
 
   elif [ "$COMP_CWORD" -eq 2 ]; then
     if ( [ "${COMP_WORDS[1]}" == "generate" ] || [ "${COMP_WORDS[1]}" == "remove" ] || [ "${COMP_WORDS[1]}" == "get-secret" ] ) ; then
-      local migrations="`'ls' ~/.local/share/totp-cli/`"
+      local migrations="`'ls' ~/.local/share/totp-cli/accounts/`"
       COMPREPLY=($(compgen -W "$migrations" -- ${COMP_WORDS[2]}))
 
     fi
